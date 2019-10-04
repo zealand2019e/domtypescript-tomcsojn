f546aaa5cf7b7a18b962f3e43188aba428075183
tomcsojn <44970244+tomcsojn@users.noreply.github.com>
2019-10-04 15:10:25 +0200

    add item

--- src/js/index.ts
+++ src/js/index.ts
@@ -15,13 +15,39 @@ p.insertBefore(newElement, document.getElementById("3"));
 let usunds: HTMLCollectionOf<Element> = document.getElementsByClassName("usundt");
 const listeUsund: HTMLElement = document.getElementById("listeUsund")
 
-console.log(usunds.length)
-console.log(usunds[0])
-console.log(usunds[1])
-console.log(usunds[2])
+
 
 for (let index = 0; index < usunds.length; index++) {
     const element = usunds[0];
     listeUsund.appendChild(element)
     console.log(element)
 }
+
+document.getElementById("3").textContent = "ALMA";
+
+//eventlistener task!
+let inputElement: HTMLInputElement = <HTMLInputElement>document.getElementById("myInput");
+document.getElementById("myBtn").addEventListener("click", addToList);
+let list: HTMLElement = document.getElementById("liste");
+let sundt: HTMLInputElement = <HTMLInputElement>document.getElementById("sundt");
+
+
+
+function addToList() {
+    let inputText = inputElement.value;
+    let newLi: HTMLElement = document.createElement("li");
+    newLi.textContent = inputText;
+
+
+    if (sundt.checked) {
+        newLi.id = list.childElementCount.toString();
+        newLi.className = "sundt"
+        list.appendChild(newLi);
+    } else {
+        newLi.id = listeUsund.childElementCount.toString();
+        newLi.className = "usundt"
+        listeUsund.appendChild(newLi);
+    }
+
+}
+
